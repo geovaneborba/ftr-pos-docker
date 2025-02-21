342b46c3b9d3e8387eac02152eb4c562d606dff8
Geovane Borba <geovanne@live.com>
2025-02-21 18:40:35 -0300

    feat: add health check route

---
+++ src/routes/health.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from "fastify";
+
+export const healthRoute = async (app: FastifyInstance) => {
+  app.get("/health", async (request, reply) => {
+    return reply.status(200).send({
+      status: "ok",
+    });
+  });
+};
