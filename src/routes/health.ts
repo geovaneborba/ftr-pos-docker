cd842b9da8ce1ee106669d9ce1f005dc2cc98a0e
Geovane Borba <geovanne@live.com>
2025-02-24 13:06:09 -0300

    chore: update health check response status message

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -3,7 +3,7 @@ import { FastifyInstance } from "fastify";
 export const healthRoute = async (app: FastifyInstance) => {
   app.get("/health", async (request, reply) => {
     return reply.status(200).send({
-      status: "ok",
+      status: "ok, ecs",
     });
   });
 };
